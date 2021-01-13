b2518a57389aab5ab946b7c761e859649a56c6ae
Tomek Rojek <tomekr@gmail.com>
2021-01-13 14:33:32 +0100

    part 25 in progress

--- src/index.js
+++ src/index.js
@@ -73,6 +73,7 @@ const typeDefs = `
 
     type Mutation {
         createUser(name: String!, email: String!, age: Int): User!
+        createPost(title: String!, body: String!, published: Boolean!, author : ID!):Post!
     }
 
     type User {
@@ -147,7 +148,24 @@ const resolvers = {
             users.push(user)
             
             return user
+        },
+        createPost(parent, args, ctx, info){
+            const userExist = users.some((user) => user.id === args.author)
+                if (!userExist) {
+                    throw new Error('User not found')
+                }
+                const post = {
+                    id: uuid(),
+                    title: args.title,
+                    body: args.body,
+                    published: args.published,
+                    author: args.author
+                }
+                posts.push(post)
+
+                return post
         }
+
     },
     Post: {
         author(parent, args, ctx, info){
