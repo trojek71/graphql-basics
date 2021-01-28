e0b537a2a7e6c030fc5a57b6fb2cdd62054f085b
Tomek Rojek <tomekr@gmail.com>
2021-01-28 14:21:42 +0100

    part 27 DONE

--- src/index.js
+++ src/index.js
@@ -142,9 +142,7 @@ const resolvers = {
            }
             const user ={
                 id: uuid(),
-                name: args.name,
-                email: args.email,
-                age: args.age
+                ...args
             }
             users.push(user)
             
@@ -157,10 +155,7 @@ const resolvers = {
                 }
                 const post = {
                     id: uuid(),
-                    title: args.title,
-                    body: args.body,
-                    published: args.published,
-                    author: args.author
+                    ...args
                 }
                 posts.push(post)
 
@@ -175,9 +170,7 @@ const resolvers = {
             }
             const comment ={
                 id: uuid(),
-                text: args.text,
-                author: args.author,
-                post: args.post
+                ...args
             }
 
             comments.push(comment)
